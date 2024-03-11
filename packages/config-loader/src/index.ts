407a933011d9562cc7ecd6ac62a6db7341cd9779
Comandeer <vepomoc@gmail.com>
2024-03-11 01:24:43 +0100

    feat(config-loader): add basic config loading logic

--- packages/config-loader/src/index.ts
+++ packages/config-loader/src/index.ts
@@ -1,1 +1,21 @@
-export default async function loadConfig(): Promise<void> {}
+import assert from 'node:assert/strict';
+import { globby } from 'globby';
+
+export default async function loadConfig( root: string ): Promise<Record<string, unknown>> {
+	const files = await globby( 'doco.config.{js,mjs}', {
+		cwd: root,
+		absolute: true
+	} );
+
+	if ( files.length === 0 ) {
+		return {};
+	}
+
+	const configPath = files[ 0 ];
+
+	assert( configPath, 'Configuration file path is defined' );
+
+	const config = await import( configPath );
+
+	return config.default;
+}
--- packages/config-loader/tests/index.ts
+++ packages/config-loader/tests/index.ts
@@ -1,6 +1,42 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve as resolvePath } from 'node:path';
 import test from 'ava';
 import loadConfig from '../src/index.js';
 
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
+const fixtureDirPath = resolvePath( __dirname, '__fixtures__' );
+const fixtures = {
+	withoutConfigFile: resolvePath( fixtureDirPath, 'without-config-file' ),
+	basic: resolvePath( fixtureDirPath, 'basic' ),
+	mjs: resolvePath( fixtureDirPath, 'mjs' ),
+	mutliple: resolvePath( fixtureDirPath, 'multiple' )
+} as const;
+
 test( 'loadConfig() is a function', ( t ) => {
 	t.is( typeof loadConfig, 'function' );
+} );
+
+test( 'loadConfig() returns configuration from doco.config.js file in the provided root directory', async ( t ) => {
+	const config = await loadConfig( fixtures.basic );
+
+	t.deepEqual( config, { foo: 'bar' } );
+} );
+
+test( 'loadConfig() returns configuration from doco.config.mjs file in the provided root directory', async ( t ) => {
+	const config = await loadConfig( fixtures.mjs );
+
+	t.deepEqual( config, { foo: 'bar' } );
+} );
+
+test( 'loadConfig() prefers doco.config.js over doco.config.mjs', async ( t ) => {
+	const config = await loadConfig( fixtures.mutliple );
+
+	t.deepEqual( config, { foo: 'bar' } );
+} );
+
+test( 'loadConfig() returns default configuration when config file ' +
+	'is not present in the provided root directory', async ( t ) => {
+	const config = await loadConfig( fixtures.withoutConfigFile );
+
+	t.deepEqual( config, {} );
 } );
