2cd563c54be9a18697ce95218a2d779cb8f25eda
Tomasz Jakut <vepomoc@gmail.com>
2024-04-09 22:31:06 +0200

    feat(parser-anubis): add parser skeleton

--- packages/parser-anubis/src/index.ts
+++ packages/parser-anubis/src/index.ts
@@ -1,3 +1,44 @@
-export default async function parse(): Promise<void> {
+import ts from 'typescript';
 
+export default async function parse(): Promise<ts.SourceFile> {
+	const code = 'console.log( \'Hello, world!\' );';
+	const compilerHost: ts.CompilerHost = {
+		fileExists: () => {
+			return true;
+		},
+		getCanonicalFileName: ( filename ) => {
+			return filename;
+		},
+		getCurrentDirectory: () => {
+			return '';
+		},
+		getDefaultLibFileName: () => {
+			return 'lib.d.ts';
+		},
+		getNewLine: () => {
+			return '\n';
+		},
+		getSourceFile: ( filename ) => {
+			return ts.createSourceFile( filename, code, ts.ScriptTarget.Latest, true );
+		},
+		readFile: () => {
+			return undefined;
+		},
+		useCaseSensitiveFileNames: () => {
+			return true;
+		},
+		writeFile: () => {
+			return null;
+		}
+	};
+	const fileName = 'test.mts';
+	const program = ts.createProgram( [
+		fileName
+	], {
+		noResolve: true,
+		target: ts.ScriptTarget.Latest
+	}, compilerHost );
+	const sourceFile = program.getSourceFile( fileName )!;
+
+	return sourceFile;
 }
--- packages/parser-anubis/tests/index.ts
+++ packages/parser-anubis/tests/index.ts
@@ -4,3 +4,9 @@ import parse from '../src/index.js';
 test( 'parse() is a function', ( t ) => {
 	t.is( typeof parse, 'function' );
 } );
+
+test( 'parse() returns TS source file', async ( t ) => {
+	const result = await parse();
+
+	t.true( 'statements' in result );
+} );
