ef7f92b773fe80bf6ad4fab1a434aa541ba2908e
Tomasz Jakut <vepomoc@gmail.com>
2024-04-29 23:01:17 +0200

    feat(core): extend the `Node` interface

--- packages/core/src/Node.ts
+++ packages/core/src/Node.ts
@@ -1,6 +1,22 @@
 export type NodeType = 'property';
 
+export interface NodePosition {
+	readonly line: number;
+	readonly column: number;
+}
+
+export interface NodeSource {
+	readonly filePath: string;
+	readonly start: NodePosition;
+	readonly end: NodePosition;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface NodeMetadata {}
+
 export interface Node {
 	readonly name: string;
 	readonly type: NodeType;
+	readonly source: NodeSource;
+	readonly metadata: NodeMetadata;
 }
