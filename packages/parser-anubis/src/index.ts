1f1907c58a32f3e8c3bc70320102843ff989be96
Comandeer <comandeer@comandeer.pl>
2024-04-30 23:13:02 +0200

    feat(parser-anubis): implement the `Parser` interface

--- packages/parser-anubis/src/index.ts
+++ packages/parser-anubis/src/index.ts
@@ -1,21 +1,43 @@
 import { readFile } from 'node:fs/promises';
+import { Node, Parser } from '@docojs/core';
 import ts from 'typescript';
 
-export default async function parse( files: Array<string> ): Promise<Array<ts.SourceFile>> {
-	const fileContents = await readFiles( files );
-	const sourceFiles = fileContents.map( ( content: string, i: number ) => {
-		const fileName = files[ i ]!;
+export default class ParserAnubis implements Parser {
+	async parse( files: ReadonlyArray<string> ): Promise<Array<Node>> {
+		const fileContents = await readFiles( files );
+		const sourceFiles = fileContents.map( ( content: string, i: number ) => {
+			const fileName = files[ i ]!;
 
-		return ts.createSourceFile( fileName, content, ts.ScriptTarget.Latest, true );
-	} );
+			return ts.createSourceFile( fileName, content, ts.ScriptTarget.Latest, true );
+		} );
 
-	return sourceFiles;
+		return sourceFiles.map( sourceFileToNode );
+	}
 }
 
-async function readFiles( files: Array<string> ): Promise<Array<string>> {
+async function readFiles( files: ReadonlyArray<string> ): Promise<Array<string>> {
 	const readPromises = files.map( ( filePath ) => {
 		return readFile( filePath, 'utf-8' );
 	} );
 
 	return Promise.all( readPromises );
+}
+
+function sourceFileToNode( sourceFile: ts.SourceFile ): Node {
+	return {
+		name: 'test',
+		type: 'property',
+		source: {
+			filePath: sourceFile.fileName,
+			start: {
+				line: 0,
+				column: 0
+			},
+			end: {
+				line: 0,
+				column: 0
+			}
+		},
+		metadata: {}
+	};
 }
--- packages/parser-anubis/tests/index.ts
+++ packages/parser-anubis/tests/index.ts
@@ -1,26 +1,24 @@
 import { dirname, resolve as resolvePath } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import test from 'ava';
-import parse from '../src/index.js';
+import ParserAnubis from '../src/index.js';
 
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 
-test( 'parse() is a function', ( t ) => {
-	t.is( typeof parse, 'function' );
+test( 'ParserAnubis is a class', ( t ) => {
+	t.is( typeof ParserAnubis, 'function' );
 } );
 
-test( 'parse() returns array of TS source file for the given files', async ( t ) => {
+test( 'parse() returns array of Nodes for the given files', async ( t ) => {
 	const fixturePaths = [
 		getFixturePath( '1-hello-world' ),
 		getFixturePath( '2-simple-export' )
 	];
-	const result = await parse( fixturePaths );
+	const parser = new ParserAnubis();
+	const result = await parser.parse( fixturePaths );
 
+	t.true( Array.isArray( result ) );
 	t.is( result.length, 2 );
-
-	for ( const file of result ) {
-		t.true( 'statements' in file );
-	}
 } );
 
 function getFixturePath( filename: string, extension = 'ts' ): string {
